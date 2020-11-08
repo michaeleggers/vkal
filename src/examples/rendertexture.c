56022b637cdd6fe06de5ce6aa81ca1f78245f8d4
Michael Eggers <eggers@hm.edu>
2020-11-08 13:39:23 +0100

    fix: use new vkal_create_texture() function

--- src/examples/rendertexture.c
+++ src/examples/rendertexture.c
@@ -239,12 +239,12 @@ int main(int argc, char ** argv)
 
     /* Texture Data */
     Image image = load_image_file("../src/examples/assets/textures/hk.jpg");
-    Texture texture = vkal_create_texture(0, image.data, image.width, image.height, image.channels, 0,
-					  VK_IMAGE_VIEW_TYPE_2D, 0, 1, 0, 1, VK_FILTER_LINEAR, VK_FILTER_LINEAR);
+    Texture texture = vkal_create_texture(0, image.data, image.width, image.height, 4, 0,
+					  VK_IMAGE_VIEW_TYPE_2D, VK_FORMAT_R8G8B8A8_UNORM, 0, 1, 0, 1, VK_FILTER_LINEAR, VK_FILTER_LINEAR);
     free(image.data);
     Image image2 = load_image_file("../src/examples/assets/textures/brucelee.jpg");
-    Texture texture2 = vkal_create_texture(0, image2.data, image2.width, image2.height, image2.channels, 0,
-					   VK_IMAGE_VIEW_TYPE_2D, 0, 1, 0, 1, VK_FILTER_LINEAR, VK_FILTER_LINEAR);
+    Texture texture2 = vkal_create_texture(0, image2.data, image2.width, image2.height, 4, 0,
+					   VK_IMAGE_VIEW_TYPE_2D, VK_FORMAT_R8G8B8A8_UNORM, 0, 1, 0, 1, VK_FILTER_LINEAR, VK_FILTER_LINEAR);
     free(image2.data);    
     vkal_update_descriptor_set_texture(descriptor_sets[0], texture);
     vkal_update_descriptor_set_texture(descriptor_sets[1], texture2);
