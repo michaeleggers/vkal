fff544ea10735611309bf4c9f40df2e6b83bc609
Michael Eggers <mseggers@protonmail.ch>
2022-06-23 14:52:09 +0200

    use c-standard lib to load files on windows

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 ### TODOs
 
-- [ ] Move duplicate image loading code from examples to /utils/*.c
-- [ ] Make shaders compile automatically in Visual Studio
-- [ ] Fix formatting of sourcecode
-- [ ] Correct description-comments in examples
-- [ ] Use STL to load files in utils for platform independence
-- [x] Replace some of the (unlicensed) textures for example programs
+- [ ] Move duplicate image loading code from examples to /utils/*.c.
+- [ ] Make shaders compile automatically in Visual Studio.
+- [ ] Fix formatting of sourcecode.
+- [ ] Correct description-comments in examples.
+- [x] Use C-Library to load files instead of native OS-specific APIs.
+- [x] Replace some of the (unlicensed) textures for example programs.
--- src/examples/utils/platform.c
+++ src/examples/utils/platform.c
@@ -12,58 +12,6 @@
 
 #include <Windows.h>
 
-	void read_file(char const * filename, uint8_t ** out_buffer, int * out_size)
-	{
-		char exe_path[256];
-		get_exe_path(exe_path, 256 * sizeof(char));
-		char abs_path[256];
-		memcpy(abs_path, exe_path, 256);
-		strcat(abs_path, filename);
-
-		HANDLE fileHandle;
-		fileHandle = CreateFile(abs_path,
-			GENERIC_READ,
-			FILE_SHARE_READ,
-			NULL,
-			OPEN_EXISTING,
-			FILE_ATTRIBUTE_NORMAL,
-			NULL
-		);
-		if (fileHandle == INVALID_HANDLE_VALUE)
-			printf("unable to open file: %s\n", abs_path);
-
-		DWORD filesize = GetFileSize(fileHandle, NULL);
-
-		*out_buffer = (uint8_t*)VirtualAlloc(
-			NULL,
-			(filesize + 1) * sizeof(uint8_t),
-			MEM_RESERVE | MEM_COMMIT,
-			PAGE_READWRITE
-		);
-		//*out_buffer = (uint8_t*)malloc((filesize + 1) * sizeof(uint8_t));
-		assert((*out_buffer != NULL) && "Failed to allocate memory for file");
-
-		struct _OVERLAPPED ov = { 0 };
-		DWORD numBytesRead = 0;
-		DWORD error;
-		if (ReadFile(fileHandle, *out_buffer, filesize, &numBytesRead, NULL) == 0)
-		{
-			error = GetLastError();
-			char errorMsgBuf[256];
-			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-				NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				errorMsgBuf, (sizeof(errorMsgBuf) / sizeof(char)), NULL);
-
-			printf("%s\n", errorMsgBuf);
-		}
-		else
-		{
-			(*out_buffer)[filesize] = '\0';
-			*out_size = filesize;
-		}
-		CloseHandle(fileHandle);
-	}
-
 	void get_exe_path(char * out_buffer, int buffer_size)
 	{
 		DWORD len = GetModuleFileNameA(NULL, out_buffer, buffer_size);
@@ -98,31 +46,7 @@
 		//}
 	}
 
-	void * initialize_memory(uint32_t size)
-	{
-		void * result = malloc(size);
-		assert((result != NULL) && "Failed to initialize memory");
-		memset(result, 0, size);
-		return result;
-	}
-
 #elif __APPLE__
-
-	void read_file(char const * filename, uint8_t ** out_buffer, int * out_size)
-	{
-		char exe_path[256];
-		get_exe_path(exe_path, 256 * sizeof(char));
-		char abs_path[256];
-		memcpy(abs_path, exe_path, 256);
-		strcat(abs_path, filename);
-		FILE * file = fopen(abs_path, "rb");
-		fseek(file, 0L, SEEK_END);
-		*out_size = ftell(file);
-		fseek(file, 0L, SEEK_SET);
-		*out_buffer = (uint8_t*)malloc(*out_size);
-		fread(*out_buffer, sizeof(char), *out_size, file);
-		fclose(file);
-	}
 
 	void get_exe_path(char * out_buffer, int buffer_size)
 	{
@@ -139,23 +63,6 @@
 #elif __linux__
 	#include <unistd.h>
 
-	void read_file(char const * filename, uint8_t ** out_buffer, int * out_size)
-	{
-		char exe_path[256];
-		get_exe_path(exe_path, 256 * sizeof(char));
-		char abs_path[256];
-		memcpy(abs_path, exe_path, 256);
-		strcat(abs_path, filename);
-		printf("abs-path: %s\n", abs_path);
-		FILE * file = fopen(abs_path, "rb");
-		fseek(file, 0L, SEEK_END);
-		*out_size = ftell(file);
-		fseek(file, 0L, SEEK_SET);
-		*out_buffer = (uint8_t*)malloc(*out_size);
-		fread(*out_buffer, sizeof(char), *out_size, file);
-		fclose(file);
-	}
-
 	void get_exe_path(char * out_buffer, int buffer_size)
 	{
 		readlink("/proc/self/exe", out_buffer, buffer_size);
@@ -170,4 +77,20 @@
 
 
 
-/* Platform independent*/
+/* Platform independent (only C-libary which _should_ work on all systems). */
+
+void read_file(char const* filename, uint8_t** out_buffer, int* out_size)
+{
+	char exe_path[256];
+	get_exe_path(exe_path, 256 * sizeof(char));
+	char abs_path[256];
+	memcpy(abs_path, exe_path, 256);
+	strcat(abs_path, filename);
+	FILE* file = fopen(abs_path, "rb");
+	fseek(file, 0L, SEEK_END);
+	*out_size = ftell(file);
+	fseek(file, 0L, SEEK_SET);
+	*out_buffer = (uint8_t*)malloc(*out_size);
+	fread(*out_buffer, sizeof(char), *out_size, file);
+	fclose(file);
+}
